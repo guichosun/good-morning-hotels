f0d432b6214188b03ba224a27a92af5f622f4f5f
alberto.delcampo-at-676770050448 <luis.albertodelcampo@gmail.com>
2024-01-24 13:38:23 -0600

    Add eureka-server's pom & main class

---
+++ eureka-server/src/main/java/com/challenge/gmh/eureka/server/EurekaServerApplication.java
@@ -0,0 +1,16 @@
+package com.challenge.gmh.eureka.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * The eureka-server.
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
